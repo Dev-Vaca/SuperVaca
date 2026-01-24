401eec8f606953864898a0e5fd2781db7476aaa6
JCVACA <jvaca5@ucol.mx>
2026-01-24 15:10:49 -0600

    Initial Commit

---
+++ SuperVaca/SuperVacaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperVacaApp.swift
+//  SuperVaca
+//
+//  Created by Julio César Vaca García on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperVacaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperVacaTests/SuperVacaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuperVacaTests.swift
+//  SuperVacaTests
+//
+//  Created by Julio César Vaca García on 24/01/26.
+//
+
+import Testing
+@testable import SuperVaca
+
+struct SuperVacaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
